de4e2609ceea640f39f1ac4a7d3d0a096d583db4
Mahmoud Aoata <mdo@Mahmouds-MacBook-Pro.local>
2023-02-05 16:29:40 +0100

    Initial Commit

---
+++ MockTests/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MockTests
+//
+//  Created by Mahmoud Aoata on 5.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MockTests/MockTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTestsApp.swift
+//  MockTests
+//
+//  Created by Mahmoud Aoata on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MockTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
